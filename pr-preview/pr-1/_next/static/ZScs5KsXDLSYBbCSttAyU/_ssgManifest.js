c32845b640e34d4d6e1f582aa630658cd8b4ce42
rastermanden <rastermanden@users.noreply.github.com>
2026-06-09 18:22:19 +0000

    Remove preview for PR 1 🛬

--- pr-preview/pr-1/_next/static/ZScs5KsXDLSYBbCSttAyU/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fkata\u002F[kataId]","\u002F[locale]\u002Fkata\u002F[kataId]\u002Fstep\u002F[stepNumber]","\u002F[locale]\u002Fkyu\u002F[kyuLevel]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
